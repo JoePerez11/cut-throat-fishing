80d9d45482326f40634916d8a6a8c55726ea236a
Joseph Perez <perez.joseph.k@gmail.com>
2022-08-18 22:18:40 -0500

    first commit

--- src/main/java/com/fishing/ctf/CtfApplication.java
+++ src/main/java/com/fishing/ctf/CtfApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CtfApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CtfApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CtfApplication.class, args);
+    }
 
 }
--- src/test/java/com/fishing/ctf/CtfApplicationTests.java
+++ src/test/java/com/fishing/ctf/CtfApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CtfApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
